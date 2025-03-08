9b4ea2fc7a668ecd04c02cf80fdb31de958a7ede
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-03-07 21:34:16 -0800

    minimum exercise in functions

---
+++ minimum.js
@@ -0,0 +1,13 @@
+function minimum (x, y){
+    if (x < y){
+        return x;
+    }
+    else if (y < x){
+        return y;
+    }
+    else {
+        return x;
+    }
+}
+
+console.log(minimum(-4, 9));
